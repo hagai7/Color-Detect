a7461f947776c5c54109e093e5bbe3b01ec8c16e
hagai7 <hagai.yehosh@gmail.com>
2025-05-20 16:21:44 +0300

    Update project before Render deployment

--- server/server.js
+++ server/server.js
@@ -44,11 +44,9 @@ app.post('/imageurl', imageController.handleApiCall);
 const path = require('path');
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Catch-all: send back index.html for any unknown route (client-side routing support)
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
